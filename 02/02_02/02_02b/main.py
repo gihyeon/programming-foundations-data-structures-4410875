a8b6ef285ccbbdf4724d78d01230c142d1c1abd9
Gihyeon Lee <gihyeon.lee@gmail.com>
2023-10-23 04:19:24 +0000

    Create a list

--- 02/02_02/02_02b/main.py
+++ 02/02_02/02_02b/main.py
@@ -2,3 +2,13 @@
 Problem Statement: Compute the average number of pets each student 
 has in a given class. 
 '''
+
+from statistics import mean
+
+student_pet_count_list = [0, 4, 2, 1, 0, 0, 2, 1]
+
+NUM_OF_STUDENTS = len(student_pet_count_list)
+print(NUM_OF_STUDENTS)
+
+average = mean(student_pet_count_list)
+print(average)
