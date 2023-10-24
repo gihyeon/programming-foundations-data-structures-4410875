421babfab4ef6a0818e7aae2607309b733fc9097
Gihyeon Lee <gihyeon.lee@gmail.com>
2023-10-24 02:16:14 +0000

    commit

--- 02/02_03/02_03b/main.py
+++ 02/02_03/02_03b/main.py
@@ -6,6 +6,19 @@ has in a given class.
 student_pet_count_list = [0, 1, 0, 2, 1, 1, 4, 0, 0, 0, 3, 2, 1, 3, 0, 2, 2, 4]
 
 NUM_OF_STUDENTS = len(student_pet_count_list)
-print(NUM_OF_STUDENTS)
+print("Number of students: ", NUM_OF_STUDENTS)
+
+ITEM_AT_INDEX_THREE = student_pet_count_list[3]
+print("Item at index three (4th item): ", ITEM_AT_INDEX_THREE)
+
+ITEM_THREE_FROM_BACK = student_pet_count_list[-3]
+print("Item three from back: ", ITEM_THREE_FROM_BACK)
+
+SUM = 0
+for individual_pet_count in student_pet_count_list:
+    SUM = SUM + individual_pet_count
+print("Sum of pet counts: ", SUM)
 
 # average = sum / number of items
+AVERAGE = SUM / NUM_OF_STUDENTS
+print("Average pet count per student: ", AVERAGE)
