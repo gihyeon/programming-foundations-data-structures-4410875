d64c7723b33af102767af4a835e9f48ac266ac3d
Gihyeon Lee <gihyeon.lee@gmail.com>
2023-10-27 12:25:56 +0000

    commit

--- 02/02_05/02_05b/main.py
+++ 02/02_05/02_05b/main.py
@@ -1,6 +1,14 @@
 seating_chart = [
-    ["Sarah", "CLaire", "Ben", "Taylor", "Eva"],
+    ["Sarah", "Claire", "Ben", "Taylor", "Eva"],
     ["Frankie", "George", "Lindsey", "Izzy", "Jack"],
     ["Katherine", "Lauren", "Mary", "Nathan", "Olive"],
     ["Chad", "April", "Matt", "Thomas", "Penny"]
 ]
+
+# Access to Lauren
+# print(seating_chart[2][1])
+
+# Display low and column no. for each students
+for i, element in enumerate(seating_chart):
+    for j, ele_val_inner_list in enumerate(element):
+        print(f"{ele_val_inner_list} is in row {i+1}, col {j+1}")
