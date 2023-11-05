5f021d7f65ac10d949f6057d49fa4f2849a628bc
Gihyeon Lee <gihyeon.lee@gmail.com>
2023-11-05 11:56:51 +0000

    commit

--- 02/02_09/02_09b/main.py
+++ 02/02_09/02_09b/main.py
@@ -1,4 +1,8 @@
 def find_second_smallest(my_list):
-    return 0
+    "Return the second samllest item in a list of integers"
+    if len(my_list) > 1:
+        my_list_sorted = sorted(my_list)
+        return my_list_sorted[1]
+    return None
 
 print(find_second_smallest([5, 8, 3, 2, 6]))
--- 02/02_10/02_10b/main.py
+++ 02/02_10/02_10b/main.py
@@ -1,4 +1,15 @@
-def find_second_smallest(my_list):
-    return 0
+def find_second_smallest_v2(my_list):
+    if len(my_list) < 2:
+        return None
+    # Initialize a variable with positive infinity that is greater than any other finite numeric value
+    smallest = float('inf')
+    second_smallest = float('inf')
+    for num in my_list:
+        if num < smallest:
+            second_smallest = smallest
+            smallest = num
+        elif num < second_smallest:
+            second_smallest = num
+    return second_smallest
 
-print(find_second_smallest([5, 8, 3, 2, 6]))
+print(find_second_smallest_v2([5, 8, 3, 2, 6]))
