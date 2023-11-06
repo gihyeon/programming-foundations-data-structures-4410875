8d3b0cc856e950b9277d584a2f48a4d1bedaa233
Gihyeon Lee <gihyeon.lee@gmail.com>
2023-11-06 14:36:09 +0000

    commit

--- 03/03_02/03_02b/main.py
+++ 03/03_02/03_02b/main.py
@@ -1,2 +1,19 @@
 # Key: State
 # Value: Capital
+
+states_to_capitals = {
+  "Texas": "Austin",
+  "New York": "Albany",
+  "California": "Sacramento",
+  "Michigan": "Lansing"
+}
+
+print(states_to_capitals["Michigan"])
+print("---")
+
+for key in states_to_capitals:
+    print(key)
+print("---")
+
+for key, value in states_to_capitals.items():
+    print(key + " | " + value)
--- 03/03_03/03_03b/main.py
+++ 03/03_03/03_03b/main.py
@@ -7,3 +7,17 @@ user_preferences = {
     "volume_level": 80,
     "date_format": "MM/DD/YYYY"
 }
+
+# Change values
+user_preferences["language"] = "Spanish"
+user_preferences["volume_level"] = 50
+
+# Add item
+user_preferences["highlight_color"] = "Yellow"
+
+# Delete item
+del user_preferences["currency"]
+removed_value = user_preferences.pop("date_format", "N/A")
+
+print(user_preferences)
+print(removed_value)
