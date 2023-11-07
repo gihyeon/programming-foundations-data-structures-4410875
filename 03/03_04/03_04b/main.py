037f7f9aa99f0192ef0bc5cd12f726b87f319d75
Gihyeon Lee <gihyeon.lee@gmail.com>
2023-11-07 00:14:34 +0000

    commit

--- 03/03_04/03_04b/main.py
+++ 03/03_04/03_04b/main.py
@@ -6,9 +6,13 @@ user_preferences = {
     "theme": None
 }
 
+# print(list(user_preferences.items()))
 
 def update_preferences(user_pref):
-    return {}
-
+    for key, value in list(user_pref.items()):
+        if value is None:
+            del user_preferences[key]
+    updated_user_pref = user_pref
+    return updated_user_pref
 
 print(update_preferences(user_preferences))
--- 03/03_05/03_05b/main.py
+++ 03/03_05/03_05b/main.py
@@ -3,11 +3,20 @@ user_preferences = {
     "language": "English",
     "notifications": None,
     "currency": "USD",
-    "theme": None
+    "theme": None,
 }
 
+# Dictionary comprehenshion
 def update_preferences(user_pref):
-    return {}
+    return {key: value for key, value in user_pref.items() if value is not None}
 
+"""
+def update_preferences(user_pref):
+    updated_preferences = {}
+    for key, value in user_pref.items():
+        if value is not None:
+            updated_preferences[key] = value
+    return updated_preferences
+"""
 
 print(update_preferences(user_preferences))
