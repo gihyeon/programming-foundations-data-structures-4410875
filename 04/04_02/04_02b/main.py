53c84a5314e3f8a340dc2c48dbe5bf9278685638
Gihyeon Lee <gihyeon.lee@gmail.com>
2023-11-09 14:58:01 +0000

    commit

--- 04/04_02/04_02b/main.py
+++ 04/04_02/04_02b/main.py
@@ -1,18 +1,18 @@
 # Set: a collection of unique items; unordered, no duplicate elements
 
-primary_colors = set(['Red', 'Blue', 'Green'])
+primary_colors = set(["Red", "Blue", "Green"])
 
 print(primary_colors)
 
-color = 'Yellow'
+COLOR = "Yellow"
 
-if color in primary_colors:
-    print(color + " is a primary color")
+if COLOR in primary_colors:
+    print(COLOR + " is a primary color")
 else:
-    print(color + " is not a primary color")
+    print(COLOR + " is not a primary color")
 
 # Add an element to a set
-letters = set(['A', 'B'])
-letters.add('C')
-letters.add('A')
+letters = set(["A", "B"])
+letters.add("C")
+letters.add("A")
 print(letters)
