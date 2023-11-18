53191d9007f980bfb3a7f7840e4448a77b3c9067
Gihyeon Lee <gihyeon.lee@gmail.com>
2023-11-18 11:16:14 +0000

    commit

--- 06/06_05/06_05b/main.py
+++ 06/06_05/06_05b/main.py
@@ -4,19 +4,22 @@
 
 from collections import deque
 
+
 def check_matching_parentheses(input_text):
     stack = deque()
     for char in input_text:
-        if char == '(':
-            stack.append('(')
-        elif char == ')':
+        if char == "(":
+            stack.append("(")
+        elif char == ")":
             if not stack:
                 return False
             stack.pop()
     return len(stack) == 0
+
 
 print(check_matching_parentheses("(This is an example)"))
 print(check_matching_parentheses("(This (is (an) example))"))
 
 print(check_matching_parentheses("This is an example)"))
 print(check_matching_parentheses("(This is an example("))
+print(check_matching_parentheses(")This is an example"))
